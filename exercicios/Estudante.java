45c8ae6e83f715d6e99456d402942e43afc1301c
ArtAxe15 <artur.pmachado1501@gmail.com>
2026-08-10 16:45:37 -0300

    Funções
    Adio=cionei as funções insereNotas, calculaMedia e menorNota na classe estudante

--- exercicios/Estudante.java
+++ exercicios/Estudante.java
@@ -6,18 +6,45 @@ public class Estudante {
 
     public Estudante (String nome){
         this.nome = nome;
-        this.notas = new ArrayList<Double>();
+        this.notas = new ArrayList<>();
     }
     
     public String getNome(){
         return nome;
     }
 
-    public void setNumero(String nome){
+    public void setNome(String nome){
         this.nome = nome;
     }
 
     public ArrayList<Double> getNotas(){
         return notas;
+    }
+
+    public void insereNotas(Double nota) {
+        if (notas.size() < 5) {
+            notas.add(nota);
+        } else {
+            System.out.println("O estudante já possui 5 notas.");
+        }
+    }
+
+    public double calculaMedia(){
+        double media = 0.0;
+        for (int i = 0; i < notas.size(); i++) {
+            media = media + notas.get(i);
+        }
+        media = media/notas.size();   
+        return media;
+    }
+    
+    public double menorNota(){
+        double menor = notas.get(0);
+        for (int i = 0; i < notas.size(); i++) {
+            if (menor > notas.get(i)){
+                menor = notas.get(i);
+            }
+        }
+        return menor;
     }
 }
