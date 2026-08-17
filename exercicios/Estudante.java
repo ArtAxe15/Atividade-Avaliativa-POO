475f8e192560178df6541e46301a72200e51bcff
ArtAxe15 <artur.pmachado1501@gmail.com>
2026-08-17 15:20:25 -0300

    Enhance Estudante class with grading methods
    Added methods to insert grades, calculate average, and find the lowest grade.

--- exercicios/Estudante.java
+++ exercicios/Estudante.java
@@ -20,4 +20,31 @@ public class Estudante {
     public ArrayList<Double> getNotas(){
         return notas;
     }
+
+    public void insereNotas(Double nota) {
+        if (notas.size() < 5) {
+            notas.add(nota);
+        } else {
+            System.out.println("O estudante já possui 5 notas.");
+        }
+    }
+
+    public double calculaMedia(){
+        double media = 0.0;
+        for (int i = 0; i < notas.size(); i++) {
+            media = media + notas.get(i);
+        }
+        media = media/notas.size();   
+        return media;
+    }
+    
+    public double menorNota(){
+        double menor = notas.get(0);
+        for (int i = 0; i < notas.size(); i++) {
+            if (menor > notas.get(i)){
+                menor = notas.get(i);
+            }
+        }
+        return menor;
+    }
 }
