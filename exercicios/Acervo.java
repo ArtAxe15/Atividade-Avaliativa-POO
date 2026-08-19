d00acbf27f2f630c51b92d2e3111aea896b2f137
ArtAxe15 <artur.pmachado1501@gmail.com>
2026-08-19 09:34:03 -0300

    Add files via upload

---
+++ exercicios/Acervo.java
@@ -0,0 +1,11 @@
+public class Acervo {
+    private String titulo;
+
+    public Acervo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
---
+++ exercicios/Livro.java
@@ -0,0 +1,23 @@
+public class Livro extends Acervo {
+
+    private boolean disponivel;
+
+    public Livro(String titulo) {
+        super(titulo);
+        this.disponivel = true;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void emprestar() {
+        if (disponivel) {
+            disponivel = false;
+        }
+    }
+
+    public void devolver() {
+        disponivel = true;
+    }
+}
